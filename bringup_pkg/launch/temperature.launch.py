b0ab2b9e9be12021dd55172ca0739b294f8fec1e
Eyiram Gaze <eyiramgaze@gmail.com>
2026-07-08 21:32:00 +0000

    added rsp, rviz2, jsp to launch file

--- bringup_pkg/launch/temperature.launch.py
+++ bringup_pkg/launch/temperature.launch.py
@@ -1,5 +1,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+from os.path import join
+
+
+DESCRIPTION_PACKAGE="parcbot_description"
+ROBOT_DESCRIPTION=join(get_package_share_directory(DESCRIPTION_PACKAGE), 'urdf', 'parcbot.urdf.xacro')
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -16,8 +24,41 @@ def generate_launch_description():
         executable="subscriber",
     )
 
+    #rviz2
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz",
+        output="screen",
+        parameters=[{
+            'use_sim_true': True
+        }]
+    )
 
-    ld.add_action(temperature_node)
-    ld.add_action(listener_node)
+    #joint_state_publisher_gui
+    jsp_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='jsp_gui'
+    )
+
+    #robot_state_publisher (rsp)
+    rsp = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name='rsp',
+        parameters=[{
+            'use_sim_time': True,
+            'robot_description': ParameterValue(
+                Command(['xacro ', ROBOT_DESCRIPTION])
+            )
+        }]
+    )
+
+    # ld.add_action(temperature_node)
+    # ld.add_action(listener_node)
+    ld.add_action(rviz)
+    ld.add_action(jsp_gui)
+    ld.add_action(rsp)
 
     return ld
