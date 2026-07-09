7fc76f20b9add8e89ee82dc49ed4f946d8e5751b
Eyiram Gaze <eyiramgaze@gmail.com>
2026-07-09 14:38:20 +0000

    added: gz_bridge, world, gazebo simulation

--- bringup_pkg/launch/temperature.launch.py
+++ bringup_pkg/launch/temperature.launch.py
@@ -1,13 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
+import os
 from os.path import join
-
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 DESCRIPTION_PACKAGE="parcbot_description"
 ROBOT_DESCRIPTION=join(get_package_share_directory(DESCRIPTION_PACKAGE), 'urdf', 'parcbot.urdf.xacro')
+BRIDGE_PARAMS=os.path.join(
+    get_package_share_directory('bringup_pkg'),
+    'config',
+    'gz_bridge.yaml'
+)
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -55,10 +62,52 @@ def generate_launch_description():
         }]
     )
 
+    default_world = os.path.join(
+        get_package_share_directory('bringup_pkg'),
+        'worlds',
+        'empty.world.sdf'
+    )
+
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+        launch_arguments={'gz_args': ['-r ', world], 'on_exit_shutdown':'true'}.items()
+    )
+
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', 'robot_description',
+                   '-name', 'parcbot'],
+        output='screen'
+    )
+
+    gz_ros_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "--ros-args",
+            "-p",
+            f"config_file:={BRIDGE_PARAMS}"
+        ]
+    )
+
     # ld.add_action(temperature_node)
     # ld.add_action(listener_node)
+    ld.add_action(world_arg)
+    ld.add_action(gazebo)
+    ld.add_action(spawn)
     ld.add_action(rviz)
     ld.add_action(jsp_gui)
     ld.add_action(rsp)
+    ld.add_action(gz_ros_bridge)
 
     return ld
